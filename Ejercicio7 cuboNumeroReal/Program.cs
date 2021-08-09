e59a1bf36665f12432a19dac71269336afb1beb3
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 20:35:06 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio7 cuboNumeroReal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio7_cuboNumeroReal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("El Resultado es :"+ NumeroCubo(3.2f));
+            Console.WriteLine("El Resultado es :" + NumeroCubo(5f));
+        }
+        public static float NumeroCubo(float Numero)
+        {
+            return Numero * Numero * Numero;
+
+        }
+    }
+}
